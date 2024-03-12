6e2d0220e62a54bf9c280da6b14e41c20298f008
Bernardo <sardinhaber@gmail.com>
2024-03-12 10:52:11 -0300

    Alteração no nome da variável

--- tabuada.js
+++ tabuada.js
@@ -1,13 +1,14 @@
-let numero = 92;
+let tabuada = 92;
 let maximo = 10;
 
-if (numero % 1 !== 0 || maximo % 1 !== 0) {
+if (tabuada % 1 !== 0 || maximo % 1 !== 0) {
     console.log("operação não realizavel com números decimais");
-} else if (numero > 0 &&  maximo > 0) {
+} else if (tabuada
+    > 0 && maximo > 0) {
     for (let i = 0; i <= maximo; i++) {
-        console.log(numero + " x " + i + " = " + i * numero);
+        console.log(tabuada + " x " + i + " = " + i * tabuada);
     }
-} else if (numero == 0 || maximo == 0) {
+} else if (tabuada == 0 || maximo == 0) {
     console.log("operação não realizavel, pois algum valor é igual a 0");
 } else {
     console.log("operação não realizavel, pois algum número é menor que 0");
