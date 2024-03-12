8c75ee5dd696c865c968b97a61090c5cd8d7e234
Bernardo <sardinhaber@gmail.com>
2024-03-12 08:32:13 -0300

    Projeto Inicial

---
+++ tabuada.js
@@ -0,0 +1,14 @@
+let tabuada = 23;
+let limite = 190;
+
+if (tabuada % 1 !== 0 || limite % 1 !== 0) {
+    console.log("operação não realizavel");
+} else if (tabuada > 0 && limite > 0) {
+    for (let i = 0; i <= limite; i++) {
+        console.log(tabuada + " x " + i + " = " + i * tabuada);
+    }
+} else if (tabuada == 0 || limite == 0) {
+    console.log("operação não realizavel, pois algum valor é igual a 0");
+} else {
+    console.log("operação não realizavel, pois algum número é menor que 0");
+}
